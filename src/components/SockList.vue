3faad4031941d9ed0bac25b9db745c91c0582539
xiaojiu5460 <672173253@qq.com>
2018-06-25 21:40:48 +0800

    list

--- src/components/SockList.vue
+++ src/components/SockList.vue
@@ -18,8 +18,11 @@
     </div>
  </div>
 </template>
-
+<script src="../../sock-price.html"></script>
 <script>
+import Vue from "vue";
+import VueResource from "vue-resource";
+Vue.use(VueResource);
 export default {
   name: "SockList",
   data() {
@@ -96,33 +99,124 @@ export default {
       hasError: false
     };
   },
-  props: {}
+  props: ['list'],
+  created: function() {
+    this.$http
+      .get(this.url)
+      .then(function(res) {
+        var sockList = this.parseData(res);
+        this.data.list = sockList; //map 从处理过的对象里取出需要的值，列入一个数组赋值给list
+      });
+  },
+  methods: {
+    parseData: function() {
+      var stock = [
+        "sh000001",
+        "sz300001",
+        "sh600180",
+        "sz000729",
+        "sz000001",
+        "sz300540",
+        "sh600150"
+      ];
+      var url = `http://web.sqt.gtimg.cn/q=${this.stock.join(",")}`;
+      var parse = str => {
+        return ("" + str)
+          .replace(/[;\s]+$/, "")
+          .split(";")
+          .map(item => {
+            let temp = {};
+            let values = ("" + item.split('="')[1]).split("~");
+            [
+              "X1",
+              "名字",
+              "代码",
+              "当前价格",
+              "昨收",
+              "今开",
+              "成交量",
+              "外盘",
+              "内盘",
+              "买一",
+              "买一量",
+              "买二",
+              "买二量",
+              "买三",
+              "买三量",
+              "买四",
+              "买四量",
+              "买五",
+              "买五量",
+              "卖一",
+              "卖一量",
+              "卖二",
+              "卖二量",
+              "卖三",
+              "卖三量",
+              "卖四",
+              "卖四量",
+              "卖五",
+              "卖五量",
+              "最近逐笔成交",
+              "时间",
+              "涨跌",
+              "涨跌%",
+              "最高",
+              "最低",
+              "价格/成交量/成交额",
+              "成交量",
+              "成交额",
+              "换手率",
+              "市盈率",
+              "X2",
+              "最高",
+              "最低",
+              "振幅",
+              "流通市值",
+              "总市值",
+              "市净率",
+              "涨停价",
+              "跌停价"
+            ].forEach(name => {
+              temp[name] = values.shift() || "";
+            });
+            temp["最近逐笔成交"] = ("" + temp["最近逐笔成交"]).split("|");
+            return temp;
+          });
+      };
+      $.get(url).done(res => {
+        console.log(res);
+        var data = parse(res);
+        console.log(data);
+      });
+    }
+  }
 };
 </script>
 
 <style scoped>
 .new div,
-.limit div{
-  width:10px;
-  padding-left:5px;
+.limit div {
+  width: 10px;
+  padding-left: 5px;
 }
-.up{
-  border-left:4px solid transparent;
+.up {
+  border-left: 4px solid transparent;
   border-right: 4px solid transparent;
-  border-bottom:5px solid #abbfe2;
+  border-bottom: 5px solid #abbfe2;
   height: 0;
   width: 0;
-  color:#abbfe2;
+  color: #abbfe2;
   display: block;
   margin-bottom: 4px;
 }
-.down{
-  border-left:4px solid transparent;
+.down {
+  border-left: 4px solid transparent;
   border-right: 4px solid transparent;
-  border-top:5px solid #abbfe2;
+  border-top: 5px solid #abbfe2;
   height: 0;
   width: 0;
-  color:#abbfe2;
+  color: #abbfe2;
   display: block;
   margin-top: 4px;
 }
