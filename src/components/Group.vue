16b6b358cce3c04abf71ce15bd9d8b304422d3d9
xiaojiu5460 <672173253@qq.com>
2018-07-09 11:54:43 +0800

    组件拆分：‘分组和颜色切换’

---
+++ src/components/Group.vue
@@ -0,0 +1,103 @@
+<template>
+  <div>
+    <div :class="{skins:true,black:notActive}" v-show="isShow"><span :class="{up:true,blackUp:notActive}"></span>
+      <ul>
+        <li><i class="iconfont icon-fenzu"></i><span>分组管理</span></li>
+        <li @click="blackColor"><i class="iconfont icon-iconset0454"></i><span>黑色皮肤</span></li>
+      </ul>
+     </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Group",
+  data() {
+    return {
+      notActive: false
+    };
+  },
+  props: ["isShow"],
+  methods: {
+    blackColor: function() {
+      this.notActive = true;
+      this.$emit("black");
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.blackUp {
+  border-left: 6px solid transparent;
+  border-right: 6px solid transparent;
+  border-bottom: 9px solid #1e232d;
+  height: 0;
+  width: 0;
+  display: block;
+  position: absolute;
+  right: 11px;
+  top: -9px;
+}
+.black {
+  position: absolute;
+  top: 45px;
+  right: 3px;
+  ul {
+    padding: 0px;
+    margin: 0px;
+    box-shadow: 2px 3px 5px #888888;
+  }
+  li {
+    list-style: none;
+    width: 128px;
+    height: 44px;
+    line-height: 44px;
+    color: #fafafa;
+    background-color: #1e232d;
+    border-bottom: 1px solid #101419;
+    text-align: center;
+    font-size: 14px;
+    font-weight: normal;
+    span {
+      padding-left: 5px;
+    }
+  }
+}
+.up {
+  border-left: 6px solid transparent;
+  border-right: 6px solid transparent;
+  border-bottom: 9px solid #fff;
+  height: 0;
+  width: 0;
+  display: block;
+  position: absolute;
+  right: 11px;
+  top: -9px;
+}
+.skins {
+  position: absolute;
+  top: 45px;
+  right: 3px;
+  ul {
+    padding: 0px;
+    margin: 0px;
+    box-shadow: 2px 3px 5px #888888;
+  }
+  li {
+    list-style: none;
+    width: 128px;
+    height: 44px;
+    line-height: 44px;
+    color: #353535;
+    border-bottom: 1px solid #f1f2f5;
+    background-color: #fff;
+    text-align: center;
+    font-size: 14px;
+    font-weight: normal;
+    span {
+      padding-left: 5px;
+    }
+  }
+}
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,112 +1,110 @@
 <template>
-  <div class="container">
+  <div :class="{container:true,blackSkin:false}">
     <div class="search">
       <div><i class="iconfont icon-fangdajing"></i>点击搜索股票/板块</div>
     </div>
-    <div class="more"><span>···</span>
-      <div class="skins" v-show="isShow"><span class="up"></span>
-       <ul>
-         <li>分组管理</li>
-         <li>黑色皮肤</li>
-        </ul>
-     </div>
+    <div class="more"><span @click="showGroup">···</span>
+    <Group :isShow="isShow" v-on:black="blackColor"></Group>
     </div>
   </div>
 </template>
 
 <script>
+import Group from "./Group.vue";
 export default {
   name: "Header",
-  data(){
-    return{
-      isShow:false,
-    }
+  components: { Group },
+  data() {
+    return {
+      isShow: false
+    };
   },
-  props: {}
+  props: {},
+  methods: {
+    showGroup: function() {
+      this.isShow = true;
+    },
+    blackColor:function(){
+      this.container=false;
+      this.blackSkin=true;
+    }
+  }
 };
 </script>
 
-<style scoped>
-.up {
-  border-left: 7px solid transparent;
-  border-right: 7px solid transparent;
-  border-bottom: 9px solid #fff;
-  height: 0;
-  width: 0;
-  display: block;
-  position: absolute;
-  right:11px;
-  top:-9px;
-}
-.skins{
-  position: absolute;
-  top:45px;
-  right: 3px;;
-}
-.skins ul {
-  padding: 0px;
-  margin: 0px;
-}
-.skins li {
-  list-style: none;
-  width: 128px;
-  height: 44px;
-  line-height: 44px;
-  color: #353535;
-  border-bottom: 1px solid #f1f2f5;
-  background-color: #fff;
-  text-align: center;
+<style scoped lang="scss">
+.blackSkin{
+  background-color: #1d2127;
+  height: 45px;
+  display: flex;
+  .search {
+    margin: 0;
+    display: flex;
+    align-items: center;
+    flex: 1;
+    padding: 0 12px;
+    div {
+      background-color: #141019;
+      height: 26px;
+      border: 0;
+      border-radius: 5px;
+      width: 100%;
+      color: #6c737f;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 12px;
+      font-family: "Microsoft Yahei";
+      letter-spacing: 1px;
+    }
+  }
+  .more {
+    display: flex;
+    align-items: center;
+    margin: 0;
+    padding: 0;
+    color: #fff;
+    width: 30px;
+    font-size: 12px;
+    font-weight: bold;
+    letter-spacing: 1px;
+  }
 }
 .container {
   background-color: #2d6bb1;
   height: 45px;
   display: flex;
-}
-img {
-  height: 15px;
-  width: 15px;
-  padding-right: 5px;
-}
-.search {
-  margin: 0;
-  display: flex;
-  align-items: center;
-  flex: 1;
-  padding: 0 12px;
-}
-
-.search div {
-  background-color: #427ab8;
-  height: 26px;
-  border: 0;
-  border-radius: 5px;
-  width: 100%;
-  color: #b8cce3;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 12px;
-  font-family: "Microsoft Yahei";
-  letter-spacing: 1px;
-}
-/* input::-webkit-input-placeholder {
-  /* WebKit browsers 
-  color: #b8cce3;
-  text-align: center;
-  font-size: 12px;
-  font-family: "Microsoft Yahei";
-  letter-spacing: 1px;
-} */
-
-.more {
-  display: flex;
-  align-items: center;
-  margin: 0;
-  padding: 0;
-  color: #fff;
-  width: 30px;
-  font-size: 12px;
-  font-weight: bold;
-  letter-spacing: 1px;
+  .search {
+    margin: 0;
+    display: flex;
+    align-items: center;
+    flex: 1;
+    padding: 0 12px;
+    div {
+      background-color: #427ab8;
+      height: 26px;
+      border: 0;
+      border-radius: 5px;
+      width: 100%;
+      color: #b8cce3;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 12px;
+      font-family: "Microsoft Yahei";
+      letter-spacing: 1px;
+    }
+  }
+  .more {
+    display: flex;
+    align-items: center;
+    margin: 0;
+    padding: 0;
+    color: #fff;
+    width: 30px;
+    font-size: 12px;
+    font-weight: bold;
+    letter-spacing: 1px;
+  }
 }
 </style>
